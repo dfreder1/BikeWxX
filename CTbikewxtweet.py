220f6bf9f8b308422682f7d2580b2e622aa38aa4
dfreder1 <dugfredericks@gmail.com>
2016-10-16 14:16:30 -0700

    file locations

--- CTbikewxtweet.py
+++ CTbikewxtweet.py
@@ -5,8 +5,8 @@ station_id = ['MSP']
 #
 dict={}
 if _platform == "linux" or _platform == "linux2":
-    e = open('/home/dougdroplet2/BikeWxX/BikeWxX/data/'+station_id[x]+'amforecast.txt','r')
-    f = open('/home/dougdroplet2/BikeWxX/BikeWxX/keys/MSPkeys.txt','r')
+    e = open('/home/dougdroplet2/projects/BikeWxX/BikeWxX/data/'+station_id[x]+'amforecast.txt','r')
+    f = open('/home/dougdroplet2/projects/BikeWxX/BikeWxX/keys/MSPkeys.txt','r')
     dict = eval(f.read())
 elif _platform == "darwin":
     e = open('data/'+station_id[x]+'amforecast.txt','r')
