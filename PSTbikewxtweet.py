73c97027c980a35875abf9b0f42fdbed2de44cf5
dfreder1 <dugfredericks@gmail.com>
2016-10-19 08:07:10 -0700

    fix bug

--- PSTbikewxtweet.py
+++ PSTbikewxtweet.py
@@ -22,8 +22,8 @@ for x in range(0,len(station_id)):
     auth = tweepy.OAuthHandler(dict['API_KEY'], dict['API_SECRET'])
     auth.set_access_token(dict['ACCESS_TOKEN'], dict['ACCESS_TOKEN_SECRET'])
     api = tweepy.API(auth)
-#   #tweetext = e.readline()+e.readline()+e.readline()
-    tweetext = 'test'
+    tweetext = e.readline()+e.readline()+e.readline()
+    #tweetext = 'test'
     print tweetext
 #
     api.update_status(tweetext) 
